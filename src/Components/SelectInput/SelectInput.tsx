a63ff14e4a8c1b86c25587206f77cd6632cccf83
Nibir Alam <nibiralpha@gmail.com>
2026-04-05 13:58:17 +0600

    code refactor on select input component

--- src/Components/SelectInput/SelectInput.tsx
+++ src/Components/SelectInput/SelectInput.tsx
@@ -1,25 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import Bar from '../Bar/Bar';
-import styles from './SelectInput.module.css'
+import React from "react";
+import styles from "./SelectInput.module.css";
 
 interface Props {
-    onSelect: (e: any) => void,
-    selected: string
+  onSelect: (e: any) => void;
+  selected: string;
 }
 
 const SelectInput = (props: Props) => {
-    const { onSelect, selected } = props
-    return (
-        <>
-            <div className={styles.select}>
-                <select value={selected} id="standard-select" onChange={onSelect}>
-                    <option value="newest">Newest</option>
-                    <option value="oldest">Oldest</option>
-                </select>
-                <span className={styles.focus}></span>
-            </div>
-        </>
-    )
-}
+  const { onSelect, selected } = props;
+  return (
+    <>
+      <div className={styles.select}>
+        <select value={selected} id="standard-select" onChange={onSelect}>
+          <option value="newest">Newest</option>
+          <option value="oldest">Oldest</option>
+        </select>
+        <span className={styles.focus}></span>
+      </div>
+    </>
+  );
+};
 
-export default SelectInput
+export default SelectInput;
