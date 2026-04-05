321badc9a0333f89a17b58c24c748868ecac8105
Nibir Alam <nibiralpha@gmail.com>
2026-04-05 14:49:25 +0600

    minor changes

--- src/route.tsx
+++ src/route.tsx
@@ -1,31 +1,28 @@
-import React, { Suspense } from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import Home from "./Screens/home/Home";
 import Artical from "./Screens/artical/Artical";
-import Layout from './Layout';
-import store from './Store/store';
-import ScrollToTop from './ScrollTop/ScrollTop';
-import Search from './Screens/Search/Search';
-
+import Layout from "./Layout";
+import store from "./Store/store";
+import ScrollToTop from "./ScrollTop/ScrollTop";
+import Search from "./Screens/Search/Search";
 const AppRoute = () => {
-
-    return (
-        <Provider store={store}>
-            <BrowserRouter>
-                <ScrollToTop />
-                <Layout>
-                    <Routes>
-                        <Route path='/' element={<Home />} />
-                        <Route path='/search' element={<Search />} />
-                        <Route path='/artical/:id' element={<Artical />} />
-                    </Routes>
-                </Layout>
-            </BrowserRouter>
-        </Provider>
-
-    )
-}
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/artical/:id" element={<Artical />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
 export default AppRoute;
