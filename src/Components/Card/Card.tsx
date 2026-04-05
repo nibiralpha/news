280020285c0ba177fe9a945617198e6a48bea7ed
Nibir Alam <nibiralpha@gmail.com>
2026-04-05 13:47:25 +0600

    card component update

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,23 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import Bar from '../Bar/Bar';
-import styles from './Card.module.css'
+import React, { useEffect, useState } from "react";
+import Bar from "../Bar/Bar";
+import styles from "./Card.module.css";
 
 interface Props {
-    children: React.ReactNode,
-    style?: object,
-    height?: string,
-    barColor?: 'red' | 'amber' | 'blue' | 'green'
+  children: React.ReactNode;
+  style?: object;
+  barColor?: "red" | "amber" | "blue" | "green";
 }
 
 const Card = (props: Props) => {
-    let { style, height, children, barColor } = props
+  let { style, children, barColor } = props;
 
-    return (
-        <div style={{ ...style }} className={styles.cardContainer}>
-            {children}
-            <Bar color={barColor}></Bar>
-        </div>
-    )
-}
+  return (
+    <div style={{ ...style }} className={styles.cardContainer}>
+      {children}
+      <Bar color={barColor}></Bar>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
