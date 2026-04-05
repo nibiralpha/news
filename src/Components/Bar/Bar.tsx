f391b0c33e36b804bf497428a0774d400a448d38
Nibir Alam <nibiralpha@gmail.com>
2026-04-05 13:44:36 +0600

    bar component update

--- src/Components/Bar/Bar.tsx
+++ src/Components/Bar/Bar.tsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import styles from './Bar.module.css'
+import styles from "./Bar.module.css";
 
 interface Props {
-    color?: 'red' | 'amber' | 'blue' | 'green'
+  color?: "red" | "amber" | "blue" | "green";
 }
 
 const Bar = (props: Props) => {
-    return (
-        <>
-            <div className={`${styles.barContainer} ${props.color ? styles[props.color] : ''}`}></div>
-        </>
-    )
-}
+  return (
+    <>
+      <div
+        className={`${styles.barContainer} ${props.color ? styles[props.color] : ""}`}
+      ></div>
+    </>
+  );
+};
 
-export default Bar
+export default Bar;
