2c30b697fc2c7d780a6c630caefcd52980fb823e
Nibir Alam <nibiralpha@gmail.com>
2026-04-20 23:07:28 +0600

    remove some comment useClickOutside

--- src/Hooks/useClickOutside.ts
+++ src/Hooks/useClickOutside.ts
@@ -5,7 +5,6 @@ const useOnClickOutside = (ref: any, handler: any) => {
   useEffect(
     () => {
       const listener = (event: any) => {
-        // Do nothing if clicking ref's element or descendent elements
 
         if (
           !ref.current ||
@@ -24,12 +23,6 @@ const useOnClickOutside = (ref: any, handler: any) => {
         document.removeEventListener("touchstart", listener);
       };
     },
-    // Add ref and handler to effect dependencies
-    // It's worth noting that because passed in handler is a new ...
-    // ... function on every render that will cause this effect ...
-    // ... callback/cleanup to run every render. It's not a big deal ...
-    // ... but to optimize you can wrap handler in useCallback before ...
-    // ... passing it into this hook.
     [ref, handler],
   );
 };
